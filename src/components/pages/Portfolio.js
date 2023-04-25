fb454178d2d02ec04cea668d709bb1ee80e43235
Halim Shini <102004913+Hshini@users.noreply.github.com>
2023-04-24 22:47:58 -0400

    Update Portfolio.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,7 +11,7 @@ export default function Portfolio() {
       <div className='gallery'>
         <a href="https://keimdm.github.io/recipe-search/" ><img src={breezyCoocktail} alt=" Breezy-Coocktail" className='bcProject' /></a>
         <a href="https://love-hungry.herokuapp.com/" ><img src={loveHungry} alt=" love-hungry" className='lhProject' /></a>
-        <a href="https://note-taker-0011.herokuapp.com/" ><img src={noteTaker} alt="note-taker" className='ntProject' /></a>
+        <a href="https://github.com/BobbyCharms/tweeter" ><img src={noteTaker} alt="tweeter" className='ntProject' /></a>
       </div>
       <footer style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
         <h1>
