86afae4b239c1b9e3ac3201e40ea947df58e44bf
Shini <halimshini@hotmail.com>
2023-04-24 23:08:54 -0400

    added tweeter project in portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import breezyCoocktail from "../../assets/images/Breezycocktail.png";
 import loveHungry from "../../assets/images/lovehungry.png";
 import noteTaker from '../../assets/images/Note-Taker.png';
+import  tweeter from '../../assets/images/tweeter.png'
 import '../../assets/css/style.css'
 import { BsLinkedin,BsGithub} from "react-icons/bs";
 
@@ -11,7 +12,7 @@ export default function Portfolio() {
       <div className='gallery'>
         <a href="https://keimdm.github.io/recipe-search/" ><img src={breezyCoocktail} alt=" Breezy-Coocktail" className='bcProject' /></a>
         <a href="https://love-hungry.herokuapp.com/" ><img src={loveHungry} alt=" love-hungry" className='lhProject' /></a>
-        <a href="https://github.com/BobbyCharms/tweeter" ><img src={noteTaker} alt="tweeter" className='ntProject' /></a>
+        <a href="https://github.com/BobbyCharms/tweeter" ><img src={tweeter} alt="tweeter" className='ntProject' /></a>
       </div>
       <footer style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
         <h1>
